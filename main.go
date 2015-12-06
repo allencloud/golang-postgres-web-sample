ff44df0799077b3cc7b2bde4a825511409c0bfa4
yxwzaxns <yxwzaxns@gmail.com>
2015-12-06 17:26:10 +0800

    alter_vars_name

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 )
 
 func main() {
-	host := os.Getenv("POSTGRES_PORT_5432_TCP_ADDR")
-	port := os.Getenv("POSTGRES_PORT_5432_TCP_PORT")
+	host := os.Getenv("POSTGRESQL_PORT_5432_TCP_ADDR")
+	port := os.Getenv("POSTGRESQL_PORT_5432_TCP_PORT")
 	username := os.Getenv("USERNAME")
 	password := os.Getenv("PASSWORD")
 
-	dbname := os.Getenv("INSTANCE_NAME")
+	dbname := os.Getenv("POSTGRESQL_INSTANCE_NAME")
 
 	connection_info := "host=" + host + " port=" + port + " user=" + username + " password=" + password + " dbname=" + dbname + " sslmode=disable"
 	fmt.Println(connection_info)
